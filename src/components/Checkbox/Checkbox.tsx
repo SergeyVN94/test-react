071c6ca3dc4315724ca73f0c592d35b9801399ff
SergeyVN <blakdgak@yandex.ru>
2020-10-23 15:44:32 +0300

    Redone checkbox.

---
+++ src/components/Checkbox/Checkbox.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { block } from 'bem-cn';
+
+import './checkbox-button.sass';
+
+interface ICheckboxProps {
+  label: string;
+  name: string;
+  checked?: boolean;
+  onChange?: (checked: boolean) => void;
+}
+
+const b = block('checkbox');
+
+const CheckboxButton: React.FC<ICheckboxProps> = (props) => {
+  const {
+    label,
+    name,
+    checked = false,
+    onChange,
+  } = props;
+
+  const [isChecked, setChecked] = React.useState(checked);
+
+  const handleChange = (): void => {
+    onChange && onChange(!isChecked);
+    setChecked(!isChecked);
+  };
+
+  return (
+    <label className={b()} >
+      <input
+        className={b('input')}
+        type='checkbox'
+        name={name}
+        checked={isChecked}
+        onChange={handleChange}
+      />
+      <span className={b('new-input')}>check</span>
+      <span className={b('label-text')}>{label}</span>
+    </label>
+  );
+};
+
+export default CheckboxButton;
