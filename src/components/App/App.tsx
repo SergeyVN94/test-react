244e8d268382256fbace8726d4189373f372b5f2
SergeyVN <blakdgak@yandex.ru>
2020-11-21 21:48:42 +0300

    Fixed store.

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,65 +5,16 @@ import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import Home from '../../pages/Home/Home';
 import EmployeePage from '../../pages/EmployeePage/EmployeePage';
 import store from '../../store/store';
-import { IEmployeeInfo } from '../../store/employees/types';
-import { loadEmployees } from '../../store/employees/actions';
-import { Role } from '../../store/filters/types';
 
-import employeesJSON from '../../data/employees.json';
-
-const baseEmployeesList: IEmployeeInfo[] = [];
-const roles: Role[] = ['cook', 'driver', 'waiter'];
-
-employeesJSON.forEach((info): boolean => {
-  if (!(/^\d+$/.test(info.id.toString()))) {
-    console.error(new TypeError(`Invalid id field. Employee information: ${JSON.stringify(info)}`));
-    return false;
-  }
-
-  if (!/^\d{2}\.\d{2}\.\d{4}$/.test(info.birthday)) {
-    console.error(new TypeError(`Incorrect birthday field for employee with id ${info.id}`));
-    return false;
-  }
-
-  const [day, month, year] = info.birthday.split('.').map((i) => parseInt(i, 10));
-  const dateOfBirth = new Date(year, month - 1, day);
-
-  const isInvalidDateOfBirth = day !== dateOfBirth.getDate()
-  || month !== dateOfBirth.getMonth() + 1
-  || year !== dateOfBirth.getFullYear();
-
-  if (isInvalidDateOfBirth) {
-    console.error(new TypeError(`Incorrect birthday field for employee with id ${info.id}`));
-    return false;
-  }
-
-  if (!roles.includes(info.role as Role)) {
-    console.error(new TypeError(`Incorrect role field for employee with id ${info.id}`));
-    return false;
-  }
-
-  baseEmployeesList.push({
-    ...info,
-    birthday: dateOfBirth,
-    role: info.role as Role,
-  });
-
-  return true;
-});
-
-const App: React.FC = () => {
-  store.dispatch(loadEmployees(baseEmployeesList));
-
-  return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/' component={ Home } />
-          <Route path='/employee/:id' component={ EmployeePage } />
-        </Switch>
-      </BrowserRouter>
-    </Provider>
-  );
-};
+const App: React.FC = () => (
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={ Home } />
+        <Route path='/employee/:id' component={ EmployeePage } />
+      </Switch>
+    </BrowserRouter>
+  </Provider>
+);
 
 export default App;
--- src/store/employees/reducer.ts
+++ src/store/employees/reducer.ts
@@ -5,10 +5,53 @@ import {
   LOAD_EMPLOYEES,
   EDIT_EMPLOYEE,
   ADD_EMPLOYEE,
+  IEmployeeInfo,
 } from './types';
+import { Role } from '../filters/types';
+import employeesJSON from '../../data/employees.json';
+
+const baseEmployeesList: IEmployeeInfo[] = [];
+const roles: Role[] = ['cook', 'driver', 'waiter'];
+
+employeesJSON.forEach((info): boolean => {
+  if (!(/^\d+$/.test(info.id.toString()))) {
+    console.error(new TypeError(`Invalid id field. Employee information: ${JSON.stringify(info)}`));
+    return false;
+  }
+
+  if (!/^\d{2}\.\d{2}\.\d{4}$/.test(info.birthday)) {
+    console.error(new TypeError(`Incorrect birthday field for employee with id ${info.id}`));
+    return false;
+  }
+
+  const [day, month, year] = info.birthday.split('.').map((i) => parseInt(i, 10));
+  const dateOfBirth = new Date(year, month - 1, day);
+
+  const isInvalidDateOfBirth = day !== dateOfBirth.getDate()
+  || month !== dateOfBirth.getMonth() + 1
+  || year !== dateOfBirth.getFullYear();
+
+  if (isInvalidDateOfBirth) {
+    console.error(new TypeError(`Incorrect birthday field for employee with id ${info.id}`));
+    return false;
+  }
+
+  if (!roles.includes(info.role as Role)) {
+    console.error(new TypeError(`Incorrect role field for employee with id ${info.id}`));
+    return false;
+  }
+
+  baseEmployeesList.push({
+    ...info,
+    birthday: dateOfBirth,
+    role: info.role as Role,
+  });
+
+  return true;
+});
 
 const employeesReducer: Reducer<IEmployeesState, EmployeesActionTypes> = (
-  state = [],
+  state = baseEmployeesList,
   action,
 ): IEmployeesState => {
   switch (action.type) {
